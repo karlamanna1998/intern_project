bbf9c0fa99fc0261a7663e8ca805325976809947
karl <karl@chipsy.in>
2024-06-25 11:03:30 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from './context/CartContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter basename="/">
       <CartProvider>
